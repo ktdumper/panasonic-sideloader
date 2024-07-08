f281616d27a3451c35d7bdb0398bb097a25dd5c8
エッケザックス <22173829+kagekiyo7@users.noreply.github.com>
2024-07-08 14:40:43 +0900

    Improve the download URL generation (#3)
    * fix the mistake
    
    * fix the mistake
    
    * Enhance the download URL generation
    
    * Delete what forgot to delete
    
    * update the sdf template

--- sideload.py
+++ sideload.py
@@ -13,7 +13,7 @@ def rm_f(path):
     except OSError:
         pass
 
-def patch_jam(jam, jar_len, adf_template):
+def patch_jam(jam, jar_len, package_url):
     config = configparser.ConfigParser()
     config.optionxform = str
 
@@ -27,6 +27,10 @@ def patch_jam(jam, jar_len, adf_template):
         except configparser.ParsingError:
             return jam
 
+    config["jam"]["PackageURL"] = package_url
+    config["jam"]["LastModified"] = "Fri, 01 Jan 2010 00:00:00"
+    config["jam"]["AppSize"] = str(jar_len)
+
     # The document says it needs to be 16 bytes or less, but the reality is different.
     if config["jam"].get("AppName") == None:
         config["jam"]["AppName"] = "No Name"
@@ -34,8 +38,6 @@ def patch_jam(jam, jar_len, adf_template):
     if config["jam"].get("AppClass") == None:
         print("WARN: no AppClass in the jam")
 
-    config["jam"]["LastModified"] = "Fri, 01 Jan 2010 00:00:00"
-    config["jam"]["AppSize"] = str(jar_len)
     config["jam"]["AccessUserInfo"] = "yes"
     config["jam"]["GetSysInfo"] = "yes"
     config["jam"]["UseTelephone"] = "call"
@@ -76,11 +78,11 @@ def patch_jam(jam, jar_len, adf_template):
 
     return config_string.getvalue()[6:].replace("\r\n", "\n").replace("\n", "\r\n").replace("\r\n\r\n", "\r\n").encode("shift-jis")
 
-def make_sdf(jam_package_url):
+def make_sdf(package_url):
     config = configparser.ConfigParser()
     config.optionxform = str
     config["sdf"] = {
-        "PackageURL": jam_package_url,
+        "PackageURL": package_url,
         "CheckCnt": "000",
         "CheckInt": "000",
         "SuspendedCnt": "000",
@@ -106,10 +108,13 @@ def make_sdf(jam_package_url):
         "AllowedLauncherApp": "any",
         "AllowedTcpHost": "any:15000",
         "AllowedUdpHost": "any:15000",
+        "LaunchBySMS": "yes",
+        "LaunchByDTV": "yes",
+        "AppID": "000000000",
         
         # Once enabled, it will be removed from the list.
-        #"LaunchBySMS": "yes",
-        #"MessageApp": "yes",
+        # "MessageApp": "yes",
+        
         "Sts": "0"
     }
 
@@ -152,35 +157,37 @@ def main():
     with open(os.path.join(input_dir, jam_path), "rb") as inf:
         jam = inf.read()
 
-    jam_package_url = re.search(r"PackageURL\s*=\s*([^\r\n]+)", jam.decode(encoding="shift-jis", errors="ignore"))
-    if jam_package_url is not None:
-        jam_package_url = jam_package_url[1]
+    if package_url := re.search(r"PackageURL\s*=\s*([^\r\n]+)", jam.decode(encoding="shift-jis", errors="ignore")):
+        package_url = package_url[1]
     else:
-        jam_package_url = "sample.jar"
+        package_url = "http://example.com/sample.jar"
         print("WARN: no PackageURL in the jam")
 
-    if jam_package_url.startswith("http"):
-        jam_url_for_jam = jam_package_url.replace(".jar", ".jam").encode("shift-jis")
-        jar_url_for_jam = jam_package_url.encode("shift-jis")
+    if package_url.startswith("http"):
+        jam_download_url = package_url.replace(".jar", ".jam").encode("shift-jis")
+        jar_download_url = package_url.encode("shift-jis")
+    elif m := re.search(r'.+?([^\r\n\/:*?"><|=]+\.jar)', package_url):
+        jam_download_url = f'http://example.com/{m[1].replace(".jar", ".jam")}'.encode("shift-jis")
+        jar_download_url = f'http://example.com/{m[1]}'.encode("shift-jis")
     else:
-        jam_url_for_jam = b"http://example.com/sample.jam"
-        jar_url_for_jam = b"http://example.com/sample.jar"
+        jam_download_url = b"http://example.com/sample.jam"
+        jar_download_url = b"http://example.com/sample.jar"
 
     adf_template = struct.pack("<I 2052s 4120s 148s 21496s 265s 2315s",
-        1, jam_url_for_jam, jar_url_for_jam, b"\x71\x01", b"\x01", b"\xFF\xFF\xFF\xFF", b"\x01")
-    jam = patch_jam(jam, os.path.getsize(os.path.join(input_dir, jar_path)), adf_template)
+        1, jam_download_url, jar_download_url, b"\x71\x01", b"\x01", b"\xFF\xFF\xFF\xFF", b"\x01")
+    jam = patch_jam(jam, os.path.getsize(os.path.join(input_dir, jar_path)), package_url)
 
     adf = bytearray(adf_template + jam)
     adf[0x1820:0x1824] = struct.pack("<I", len(jam))
     
-    sdf = make_sdf(jam_package_url)
+    sdf = make_sdf(package_url)
 
     target = None
 
     # For P-01F. Verified.
     max_appli = 100
 
-    for x in range(max_appli-1):
+    for x in range(max_appli):
         if not os.path.exists(os.path.join(output_dir, str(x))):
             target = x
             break
